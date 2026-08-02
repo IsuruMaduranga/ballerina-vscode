3cb3aca5f63d163860ad09c691eb2608589cbb1d
madushajg <madushajg@gmail.com>
2026-08-02 15:55:51 +0530

    fix: skip tests related to type editing due to unresolved issue

--- packages/ballerina-extension/e2e-test/e2e-playwright-tests/type-editor/type-explorer-navigation.spec.ts
+++ packages/ballerina-extension/e2e-test/e2e-playwright-tests/type-editor/type-explorer-navigation.spec.ts
@@ -181,7 +181,9 @@ export default function createTests() {
             await verifyGeneratedSource('types.bal', expectedFilePath, substitutions);
         });
 
-        test('Edit Type Name via Node Menu', async ({ }, testInfo) => {
+        // Skipped due to: https://github.com/wso2/product-integrator/issues/1988
+        // TODO: Enable the following test once the issue is resolved.
+        test.skip('Edit Type Name via Node Menu', async ({ }, testInfo) => {
             const testAttempt = testInfo.retry + 1;
             const addressName = `Address${testAttempt}`;
             const locationName = `Location${testAttempt}`;
